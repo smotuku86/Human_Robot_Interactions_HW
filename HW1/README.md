e3a295e2ee4b8f017059d08b911c90d3c07832ba
smotuku86 <191709755+smotuku86@users.noreply.github.com>
2026-02-08 22:50:58 -0500

    HW1 Addition

---
+++ HW1/README.md
@@ -0,0 +1,42 @@
+# HW1
+
+Dylan Losey, Virginia Tech.
+
+In this homework assignment we will record and replay demonstrations on a robot arm.
+
+## Install and Run
+
+```bash
+
+# Download
+git clone https://github.com/vt-hri/HW1.git
+cd HW1
+
+# Create and source virtual environment
+# If you are using Mac or Conda, modify these two lines as shown in [HW0](https://github.com/vt-hri/HW0)
+python3 -m venv venv
+source venv/bin/activate
+
+# Install dependencies
+# If you are using Mac or Conda, modify this line as shown in [HW0](https://github.com/vt-hri/HW0)
+pip install numpy pybullet
+
+# Run the script
+python main.py
+```
+
+## Expected Output
+
+Use your keyboard to teleoperate the robot.
+See `teleop.py` to understand which keys map to which axis of robot motion.
+<img src="env.gif" width="750">
+
+## Assignment
+
+Modify the provided code to complete the following steps:
+1. Implement a limiter to prevent users from teleoperating the robot into the table.
+2. Implement a script so that users can "record" a state by pressing the toggle button. The recorded states should be saved to a JSON file (or similar output).
+3. Provide and record a demonstration lifting one block and placing it on the other.
+4. Create a new script (called `replay.py`) that will replay the recorded trajectory. This file should load the JSON file and control the robot to move to each of the recorded states.
+5. In `replay.py`, change the location of one of the blocks. Purely replaying the recorded trajectory should now fail (since the block location has changed). Enable the user to intervene and modify the robot's motion.
+6. Your final version of `replay.py` should control the robot to follow the keypoints saved in the JSON file. The user should be able to press a button and teleoperate the robot. When the user is done, the robot should resume its original trajectory (while skipping the modified parts).
---
+++ HW1/main.py
@@ -0,0 +1,106 @@
+import pybullet as p
+import pybullet_data
+import numpy as np
+import os
+import time
+import json
+from robot import Panda
+from teleop import KeyboardController
+
+
+# parameters
+control_dt = 1. / 240.
+
+# create simulation and place camera
+physicsClient = p.connect(p.GUI)
+p.setGravity(0, 0, -9.81)
+# disable keyboard shortcuts so they do not interfere with keyboard control
+p.configureDebugVisualizer(p.COV_ENABLE_KEYBOARD_SHORTCUTS, 0)
+p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
+p.resetDebugVisualizerCamera(cameraDistance=1.0, 
+                                cameraYaw=40.0,
+                                cameraPitch=-30.0, 
+                                cameraTargetPosition=[0.5, 0.0, 0.2])
+
+# load the objects
+urdfRootPath = pybullet_data.getDataPath()
+plane = p.loadURDF(os.path.join(urdfRootPath, "plane.urdf"), basePosition=[0, 0, -0.625])
+table = p.loadURDF(os.path.join(urdfRootPath, "table/table.urdf"), basePosition=[0.5, 0, -0.625])
+cube1 = p.loadURDF(os.path.join(urdfRootPath, "cube_small.urdf"), basePosition=[0.6, -0.2, 0.05])
+cube2 = p.loadURDF(os.path.join(urdfRootPath, "cube_small.urdf"),
+                    basePosition=[0.7, 0.2, 0.05], baseOrientation=p.getQuaternionFromEuler([0, 0, 0.7]))
+
+# load the robot
+jointStartPositions = [0.0, 0.0, 0.0, -2*np.pi/4, 0.0, np.pi/2, np.pi/4, 0.0, 0.0, 0.04, 0.04]
+panda = Panda(basePosition=[0, 0, 0],
+                baseOrientation=p.getQuaternionFromEuler([0, 0, 0]),
+                jointStartPositions=jointStartPositions)
+
+# teleoperation interface
+teleop = KeyboardController()
+
+# run simulation
+# you can teleoperate the robot using the keyboard;
+# see "teleop.py" for the mapping between keys and motions
+state = panda.get_state()
+target_position = state["ee-position"]
+target_quaternion = state['ee-quaternion']
+
+gripper_open = True
+z_buffer = .02  # minimum height of end-effector to avoid table collision
+toggle_time = time.time()
+recorded_states = []
+
+while True:
+    #get current time
+    current_time = time.time()
+
+    # update the target pose
+    action = teleop.get_action()
+
+    #Check for table interference
+    if target_position[2] + action[2] < z_buffer:
+        action[2] = 0
+    
+    target_position = target_position + action[0:3] 
+
+    target_quaternion = p.multiplyTransforms([0, 0, 0], p.getQuaternionFromEuler(action[3:6]),
+                                                [0, 0, 0], target_quaternion)[1]
+    # move to the target pose
+    panda.move_to_pose(ee_position=target_position, ee_quaternion=target_quaternion)
+
+    # open or close the gripper
+    if action[6] == +1:
+        panda.open_gripper()
+        gripper_open = True
+    elif action[6] == -1:
+        panda.close_gripper()
+        gripper_open = False
+
+    # print when "." is pressed - toggle recording
+    # add debounce to avoid multiple toggles within a short time
+    recording_state = False
+    if action[7] == +1 and toggle_time - current_time > 0.5:
+        toggle_time = time.time()
+        if not recording_state:
+            recording_state = True
+            print("Recording toggled ON.")
+        else:
+            recording_state = False
+            print("Recording toggled OFF.")
+
+    if recording_state:
+        #record the robot's state or actions
+        state = panda.get_state()
+        ee_position = state["ee-position"]
+        ee_quaternion = state["ee-quaternion"]
+        recorded_state = [ee_position, ee_quaternion, gripper_open]
+        recorded_states.append(recorded_state)
+        #write to a file in json
+        with open("recorded_data.json", "w") as f:
+            json.dump(recorded_states, f)
+        print(recorded_state)  # Placeholder for actual recording logic
+
+    # step simulation
+    p.stepSimulation()
+    time.sleep(control_dt)
