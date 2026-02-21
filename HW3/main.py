03132f67de2b85e7a29a662880a9c96d14badb13
smotuku86 <191709755+smotuku86@users.noreply.github.com>
2026-02-21 00:17:24 -0500

    HW3 Done

--- HW3/main.py
+++ HW3/main.py
@@ -69,7 +69,7 @@ def angle_diff(a, b):
     # returns smallest signed angle difference in [-pi, pi]
     return np.arctan2(np.sin(a - b), np.cos(a - b))
 
-def predict_goal(initial_robot_state, current_robot_state, goals, prior, beta = 5.0, rot_scale = 0.1):
+def predict_goal(initial_robot_state, current_robot_state, goals, beta = 5.0, rot_scale = 0.1):
     #predict what object the user is trying to get 
     #Returns a dict with the chance of each object being the goal
     #initial_robot_state and current_robot_state are both lists of length 4, with the first 3 values being the position and the last being rotation around z (euler)
@@ -94,17 +94,17 @@ def predict_goal(initial_robot_state, current_robot_state, goals, prior, beta = 
         robot_distance_travelled = np.linalg.norm(current_robot_state_position - initial_robot_state_position)
 
         #for angle error
-        obj_angle_error_current = abs(np.cos(angle_diff(obj_state_rotz, current_robot_state_rotz))) 
+        obj_angle_error_current = abs(np.sin(angle_diff(obj_state_rotz, current_robot_state_rotz))) 
         #checks to see how parallel the robot is to the object - if they are parallel, this value will be 1, if they are perpendicular it will be 0
         #we want it to be perpendiculer so we can grasp it
 
-        goal_predictions[obj_name] = prior[obj_name] * np.exp( beta * (obj_distance_initial - robot_distance_travelled - obj_distance_current) ) + rot_scale* np.exp( beta * obj_angle_error_current )
-    
+        goal_predictions[obj_name] =  np.exp( beta * (obj_distance_initial - robot_distance_travelled - obj_distance_current) ) + rot_scale * np.exp( beta * obj_angle_error_current )
+        #print(f"Raw prediction for {obj_name}: {goal_predictions[obj_name]:.4f} (Distance initial: {obj_distance_initial:.4f}, Distance current: {obj_distance_current:.4f}, Robot distance travelled: {robot_distance_travelled:.4f}, Angle error current: {obj_angle_error_current:.4f})")
+        
     #normalize probabilities
     total = sum(goal_predictions.values())
     for obj_name in goal_predictions.keys():
         goal_predictions[obj_name] = goal_predictions[obj_name] / total
-        
     return goal_predictions
 
 
@@ -143,6 +143,17 @@ panda = Panda(basePosition=[0, 0, 0],
 # teleoperation interface
 teleop = KeyboardController()
 
+#for text
+debug_text_id = None
+robot_mode_id = None
+autonomy_control_id = None
+goal_prediction_id = None
+#to see if user commanded robot movement
+DidUserAct = False
+#autonomy toggle to stop robot from moving on its own
+autonomy_toggled = False
+toggle_time = time.time()
+
 # run simulation
 # you can teleoperate the robot using the keyboard;
 # see "teleop.py" for the mapping between keys and motions
@@ -155,11 +166,51 @@ init_robot_state = list(init_position) + [init_euler[2]]
 target_position = state["ee-position"]
 target_quaternion = state['ee-quaternion']
 
-prior = {"box": 1/3, "banana": 1/3, "bottle": 1/3} # smt to start code off w 
+#Code to initialize text for on screen
+
+#goal prediction
+goal_prediction_ids = {}
+object_colors = {
+    "box": [1, 0, 0],       # red
+    "banana": [1, 1, 0],    # yellow
+    "bottle": [0, 0, 1]     # blue
+}
+current_goal_texts = {obj: "" for obj in ["box", "banana", "bottle"]}
+objects_dict = {"box": box, "banana": banana, "bottle": bottle}
+
+for obj in ["box", "banana", "bottle"]:
+    goal_prediction_ids[obj] = p.addUserDebugText(
+        f"{obj}: 0.0",
+        [0, 0, 0.8 - 0.05*["box","banana","bottle"].index(obj)], # stagger z so text doesn't overlap
+        textColorRGB=object_colors[obj],
+        textSize=1.2
+    )
+    current_goal_texts[obj] = f"{obj}: 0.0"
+
 
 while True:
+
+    #get current time
+    current_time = time.time()
+
+    #remove previous text if there is
+    if robot_mode_id is not None:
+        p.removeUserDebugItem(robot_mode_id)
+    if autonomy_control_id is not None:
+        p.removeUserDebugItem(autonomy_control_id)
+    if debug_text_id is not None:
+        p.removeUserDebugItem(debug_text_id)     
+    if goal_prediction_id is not None:
+        p.removeUserDebugItem(goal_prediction_id)
+
+
+
     # update the target pose
     action = teleop.get_action()
+    #print("action: ", action)
+    #check for user input
+    DidUserAct = any(x != 0 for x in action)
+
     # this is where the human wants to go
     human_position = target_position + action[0:3]
     human_quaternion = p.multiplyTransforms([0, 0, 0], p.getQuaternionFromEuler(action[3:6]),
@@ -172,17 +223,13 @@ while True:
     robot_quaternion = state['ee-quaternion']
     robot_euler = state['ee-euler']
     current_robot_state = list(robot_position) + [robot_euler[2]]
-
-
-    #print("robot position: ", current_robot_state)
     
     # share autonomy between human and robot
     #Step 1: Predict which object the user is trying to get to
     goals = get_object_goals()
     #print("goals: ", goals)
-    goal_predictions = predict_goal(init_robot_state, current_robot_state, goals, prior, beta=1.0, rot_scale=0.0)
-    prior = goal_predictions #update prior for next round of predictions
-    print("goal predictions: ", goal_predictions)
+    goal_predictions = predict_goal(init_robot_state, current_robot_state, goals, beta=15, rot_scale=0.0)
+    #print("goal predictions: ", goal_predictions)
 
     #step 2: get robot action to reach each predicted goal
     object_actions = get_object_actions(robot_position, robot_euler, goals)
@@ -190,19 +237,68 @@ while True:
 
     #find highest probability goal and corresponding robot action
     best_goal = max(goal_predictions, key=goal_predictions.get)
-    print("best goal: ", best_goal)
+    #print("best goal: ", best_goal)
     #choose our robot action
     robot_action = []
     if goal_predictions[best_goal] > 0.5: #only assist if we are more than 50% sure of the goal
         robot_action = object_actions[best_goal]
-        print("assisting toward ", best_goal)
+
+        debug_text_id = p.addUserDebugText( f"Assisting toward {best_goal}",
+        [0, 0, 1],
+        textColorRGB=[1,0,0],
+        textSize=1.5)
+
     else: #stay in position - do not move
         robot_action = (robot_position, robot_quaternion) 
-        print('idk')
-    print("robot action: ", robot_action)
 
-    #simple alpha of .5 half robot/half human control
-    alpha = 0.5
+        debug_text_id = p.addUserDebugText( f"Goal Unsure",
+        [0, 0, 1],
+        textColorRGB=[1,0,0],
+        textSize=1.5)
+
+    #alpha tells us how much control the robot has vs the human 
+    #I'm going to implement a policy that give robot control if human is not pressing any keys
+    #and the human  control when they are pressing keys
+    # this is simple but good becasue there is text that lets the user know what goal the robot
+    #thinks the user wants. So, when the user sees the robot say it think it wants the goal they want, 
+    # they can stop commanding movement and let the robot do the rest
+
+    #toggle autonomy on/off when user presses "."
+    if action[7] == +1 and current_time - toggle_time > 0.5:
+        toggle_time = time.time() 
+        autonomy_toggled = not autonomy_toggled
+
+    #determine alpha based on autonomy and user input
+    if autonomy_toggled:
+        #when autonomy is on, give human control when they press keys, robot control otherwise
+        if DidUserAct:
+            alpha = 0.0
+        else:
+            alpha = 1.0
+        autonomy_control_id = p.addUserDebugText( f"Autonomy On",
+        [0, 0, 1.4],
+        textColorRGB=[1,0,0],
+        textSize=1.5)
+    else:
+        alpha = 0.0 #if autonomy is toggled off, human has full control
+        autonomy_control_id = p.addUserDebugText( f"Autonomy Off",
+        [0, 0, 1.4],
+        textColorRGB=[1,0,0],
+        textSize=1.5)
+
+    #display robot control mode
+    if DidUserAct:
+        robot_mode_id = p.addUserDebugText( f"User Control",
+        [0, 0, 1.2],
+        textColorRGB=[1,0,0],
+        textSize=1.5)
+    else:
+        robot_mode_id = p.addUserDebugText( f"Robot Full Control",
+        [0, 0, 1.2],
+        textColorRGB=[1,0,0],
+        textSize=1.5)
+        
+        
    
    # blending human robot control
     target_position = (1-alpha) * human_position + alpha * np.array(robot_action[0])
@@ -221,10 +317,19 @@ while True:
     elif action[6] == -1:
         panda.close_gripper()
 
-    # print when "." is pressed
-    if action[7] == +1:
-        print("button pressed")
-
+    #show goal predictions as text - update text each loop
+    for obj_name, prob in goal_predictions.items():
+        new_text = f"{obj_name}: {prob:.2f}"
+        # only update if changed to avoid flashing
+        if new_text != current_goal_texts[obj_name]:
+            p.removeUserDebugItem(goal_prediction_ids[obj_name])
+            goal_prediction_ids[obj_name] = p.addUserDebugText(
+                new_text,
+                objects_dict[obj_name].get_state()['position'] + np.array([0, 0, 0.1]), # position text above object
+                textColorRGB=object_colors[obj_name],
+                textSize=1.2
+            )
+            current_goal_texts[obj_name] = new_text
     # step simulation
     p.stepSimulation()
     time.sleep(control_dt)
